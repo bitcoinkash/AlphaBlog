39ebf3fd0951f56c44a78eb27b5b559282ef717a
SUHAIL PARRY <Suhail.parry@mail.mcgill.ca>
2024-03-17 01:33:30 -0400

    gemfile errors fixed

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,7 +22,7 @@ class ArticlesController < ApplicationController
             flash[:notice] = "Article was created successfully"
             redirect_to @article
         else
-            render 'new'
+            render :new, status: :unprocessable_entity
         end
     
 
